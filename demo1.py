1cfb5e42ad63e0b16cacd482ce27036917c79756
wangwenwei <www_1350@163.com>
2017-04-02 13:13:02 +0800

    @add demo

--- demo1.py
+++ demo1.py
@@ -4,7 +4,7 @@ import tushare as ts
 def main():
     print ts.get_hist_data('600848')
     print ts.get_hist_data('600848',start='2015-01-05',end='2015-01-09')
-    print ts.get_h_data('002337').encode('GBK')  # 前复权
+    print ts.get_h_data('002337')  # 前复权
     print ts.get_h_data('002337', autype='hfq')  # 后复权
     print ts.get_h_data('002337', autype=None)  # 不复权
     print ts.get_h_data('002337', start='2015-01-01', end='2015-03-16')  # 两个日期之间的前复权数据
---
+++ demo2.py
@@ -0,0 +1,9 @@
+#-*-coding:utf-8-*-
+import tushare as ts
+
+def main():
+    print ts.get_today_all()
+
+
+if __name__ == '__main__':
+    main()
